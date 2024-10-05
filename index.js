63b3a51cbbf1caea3f022ff1ed6a222b2351c84a
Sujit Ray <157118874+sujitrai1@users.noreply.github.com>
2024-10-05 13:18:31 +0530

    gess-number
    This is a game for using enjoyment

---
+++ index.js
@@ -0,0 +1,26 @@
+
+let b = Math.floor(Math.random() * 10 + 1);
+
+
+let attamp = 0;
+ 
+document.getElementById("guesbtn").onclick = function(){
+    let a = document.getElementById("guessNumber").value;
+
+  
+ if(a==b){
+    alert ("Congraculation !! You Right Choise" +" "+ attamp);
+   
+
+ }else if (a > b){
+    attamp++;
+    alert(" Try Again!! Lass Number");
+  
+ }
+ else{
+    attamp++;
+    alert(" Try Again!! Greater Number");
+ }
+
+}
+    
